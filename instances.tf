896c13ab5da80b257fd18b1d21a8383e7a2a87cb
Mauri <mauricampodonico@outlook.com>
2023-11-26 14:01:18 +0000

    Nueva infraestructura

--- instances.tf
+++ instances.tf
@@ -1,112 +1,78 @@
-resource "aws_instance" "bastion_host_1" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_publica_1.id
-  vpc_security_group_ids = [aws_security_group.sg_bastion.id]
-  key_name               = aws_key_pair.key_pair.key_name
-
-
-  tags = {
-    Name = "Bastion Host 1"
-  }
-}
-
-resource "aws_instance" "bastion_host_2" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_publica_2.id
-  vpc_security_group_ids = [aws_security_group.sg_bastion.id]
-  key_name               = aws_key_pair.key_pair.key_name
-
-  tags = {
-    Name = "Bastion Host 2"
-  }
-}
-
-resource "aws_instance" "web_server_1" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_privada_1.id
-  vpc_security_group_ids = [aws_security_group.sg_web.id]
-  key_name               = aws_key_pair.key_pair.key_name
-
-  tags = {
-    Name = "Web Server 1"
-  }
-}
-
-resource "aws_instance" "web_server_2" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
-  instance_type          = "t2.micro"
-  subnet_id              = aws_subnet.subnet_privada_2.id
-  vpc_security_group_ids = [aws_security_group.sg_web.id]
-  key_name               = aws_key_pair.key_pair.key_name
-
-  tags = {
-    Name = "Web Server 2"
-  }
-}
-
 resource "aws_key_pair" "key_pair" {
   key_name   = "id_rsa.pub"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzMeLrNAK40kahNSY5MVQ3dIKeHqWXD/uXrkL6dUmW/R7q4FUfgpUESAfPLQVbw+UVtvJcNjVe6OTZk3U4S7L0J0vRw6U+LClYJmqOV62HePDiD2/Xvs6AAhf4Kh+E3ExjBI8afRQQFnGXjeV25OULeRk4vcc2PBgPMJj3yw88Ld6qWpca2OCQ0bSKFv5tYm4/ZnHC9KcFTPjCpErxDTa2S6TvtZ0jmhthBdw9TEJuTA3YxLOXBTUUPJ9nm9YDXycAMzTnhCtgsTzXLYAN902xmT2aZndGKpKAdduVaxpULOGMGzSuZ930prohrA21lz5fTYrgq4iSHBs1OtS9DYZ4Q1xwo39bHbbeqnscEV0Q8YGOt4S+sH8wUbWR5uHdgzYHqw8uVqtwnzo03jpk7O8AFMuNnL54CQZUEurhpE6il8PbGlvFCF+E9N+uQUlRaBTUCMGccbm9Vid6VizJZNCmQvBPoS4pRlvW6NavA/OHfblBi2BsC7VP6RclwQT7Ikc= srd@obli-srd"
 }
 
-resource "aws_lb_target_group" "web-servers" {
-  name     = "web-servers"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc-srd.id
+resource "aws_instance" "bastion" {
+    ami           = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    key_name      = aws_key_pair.key_pair.key_name
+    subnet_id     = aws_subnet.subnet_publica.id
+    vpc_security_group_ids = [aws_security_group.sg_bastion.id]
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    interval            = 30
-    path                = "/"
-    port                = "traffic-port"
-  }
-
-  tags = {
-    Name = "TG_web-servers"
-  }
+    tags = {
+        Name = "Bastion"
+    }
 }
 
-resource "aws_lb" "ALB" {
-  name               = "ALB"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.sg_alb.id]
-  subnets            = [aws_subnet.subnet_publica_1.id, aws_subnet.subnet_publica_2.id]
+resource "aws_instance" "webserver" {
+    ami           = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    key_name      = aws_key_pair.key_pair.key_name
+    subnet_id     = aws_subnet.subnet_publica.id
+    vpc_security_group_ids = [aws_security_group.sg_web.id]
 
-  tags = {
-    Name = "ALB"
-  }
-}
-//FALTA CREAR EL LISTENER PARA HTTPS
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.ALB.arn
-  port              = 80
-  protocol          = "HTTP"
+    user_data = <<-EOF
+                            #!/bin/bash
+                            sudo apt update -y
+                            sudo apt install apache2 -y
+                            sudo systemctl enable apache2
+                            sudo systemctl start apache2
+                            sudo apt install mysql-server php libapache2-mod-php php-mysql -y
+                            wget https://wordpress.org/latest.tar.gz
+                            tar xzvf latest.tar.gz
+                            sudo cp -R wordpress/* /var/www/html/
+                            EOF
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web-servers.arn
-  }
-
-  tags = {
-    Name = "ALB_Listener"
-  }
+    tags = {
+        Name = "WebServer"
+    }
 }
 
-resource "aws_lb_target_group_attachment" "Web1" {
-  target_group_arn = aws_lb_target_group.web-servers.arn
-  target_id        = aws_instance.web_server_1.id
-  port             = 80
+resource "aws_db_instance" "WP_DB" {
+    allocated_storage    = 20
+    storage_type         = "gp2"
+    engine               = "mysql"
+    engine_version       = "5.7"
+    instance_class       = "db.t2.micro"
+    identifier           = "srd"
+    username             = "Obli.SRD"
+    password             = "Obli.SRD-2023"
+    parameter_group_name = "default.mysql5.7"
+    vpc_security_group_ids = [aws_security_group.sg_rds.id]
+    db_subnet_group_name = "db_subnet_group" 
+
+    tags = {
+        Name = "WP_DB"
+    }
 }
 
-resource "aws_lb_target_group_attachment" "Web2" {
-  target_group_arn = aws_lb_target_group.web-servers.arn
-  target_id        = aws_instance.web_server_2.id
-  port             = 80
+resource "null_resource" "configurar_wordpress" {
+    depends_on = [aws_instance.webserver, aws_db_instance.WP_DB]
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo sed -i 's/database_name_here/srd/g' /var/www/html/wp-config.php",
+            "sudo sed -i 's/username_here/Obli.SRD/g' /var/www/html/wp-config.php",
+            "sudo sed -i 's/password_here/Obli.SRD-2023/g' /var/www/html/wp-config.php",
+            "sudo sed -i 's/localhost/${aws_db_instance.WP_DB.endpoint}/g' /var/www/html/wp-config.php"
+        ]
+
+        connection {
+            type        = "ssh"
+            user        = "ubuntu"
+            private_key = file("~/.ssh/id_rsa")
+            host        = aws_instance.webserver.public_ip
+        }
+    }
 }
--- network.tf
+++ network.tf
@@ -1,302 +1,102 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"
 }
 
-resource "aws_vpc" "vpc-srd" {
-  cidr_block = "192.168.0.0/16"
+resource "aws_vpc" "vpc_srd" {
+    cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "VPC-SRD"
-  }
+    tags = {
+        Name = "vpc-srd"
+    }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc-srd.id
-
-  tags = {
-    Name = "IGW-SRD"
-  }
+    vpc_id = aws_vpc.vpc_srd.id
 }
 
-resource "aws_subnet" "subnet_publica_1" {
-  vpc_id                  = aws_vpc.vpc-srd.id
-  cidr_block              = "192.168.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+resource "aws_subnet" "subnet_publica" {
+    vpc_id     = aws_vpc.vpc_srd.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
 
-  tags = {
-    Name = "Subnet-Publica-1"
-  }
+    tags = {
+        Name = "subnet-publica"
+    }
 }
 
-resource "aws_subnet" "subnet_publica_2" {
-  vpc_id                  = aws_vpc.vpc-srd.id
-  cidr_block              = "192.168.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
+resource "aws_subnet" "subnet_privada" {
+    vpc_id     = aws_vpc.vpc_srd.id
+    cidr_block = "10.0.2.0/24"
 
-  tags = {
-    Name = "Subnet-Publica-2"
-  }
+    tags = {
+        Name = "subnet-privada"
+    }
 }
 
-resource "aws_subnet" "subnet_privada_1" {
-  vpc_id            = aws_vpc.vpc-srd.id
-  cidr_block        = "192.168.3.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_db_subnet_group" "db_subnet_group" {
+    name       = "db_subnet_group"
+    subnet_ids = [aws_subnet.subnet_privada.id]
 
-  tags = {
-    Name = "Subnet-Privada-1"
-  }
+    tags = {
+        Name = "DB Subnet Group"
+    }
 }
 
-resource "aws_subnet" "subnet_privada_2" {
-  vpc_id            = aws_vpc.vpc-srd.id
-  cidr_block        = "192.168.4.0/24"
-  availability_zone = "us-east-1b"
+resource "aws_route_table" "tabla_ruta_publica" {
+    vpc_id = aws_vpc.vpc_srd.id
 
-  tags = {
-    Name = "Subnet-Privada-2"
-  }
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "tabla_ruta_publica"
+    }
 }
 
-resource "aws_eip" "eip_nat_gw_1" {
-  vpc = true
-
-  tags = {
-    Name = "EIP-NAT-GW-1"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gw_1" {
-  subnet_id     = aws_subnet.subnet_publica_1.id
-  allocation_id = aws_eip.eip_nat_gw_1.id
-
-  tags = {
-    Name = "NAT-GW-1"
-  }
-}
-
-resource "aws_eip" "eip_nat_gw_2" {
-  vpc = true
-
-  tags = {
-    Name = "EIP-NAT-GW-2"
-  }
-}
-
-resource "aws_nat_gateway" "nat_gw_2" {
-  subnet_id     = aws_subnet.subnet_publica_2.id
-  allocation_id = aws_eip.eip_nat_gw_2.id
-
-  tags = {
-    Name = "NAT-GW-2"
-  }
-}
-
-resource "aws_route_table" "publica" {
-  vpc_id = aws_vpc.vpc-srd.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  #   route {
-  #     cidr_block = "192.168.0.0/16"
-  #     gateway_id = aws_internet_gateway.igw.id
-  #   }
-
-  tags = {
-    Name = "Tabla-de-rutas-Publica"
-  }
-}
-
-resource "aws_route_table_association" "publica_1" {
-  subnet_id      = aws_subnet.subnet_publica_1.id
-  route_table_id = aws_route_table.publica.id
-}
-
-resource "aws_route_table_association" "publica_2" {
-  subnet_id      = aws_subnet.subnet_publica_2.id
-  route_table_id = aws_route_table.publica.id
-}
-
-resource "aws_route_table" "privada_1" {
-  vpc_id = aws_vpc.vpc-srd.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw_1.id
-  }
-
-  #   route {
-  #     cidr_block = "192.168.0.0/16"
-  #     gateway_id = aws_internet_gateway.igw.id
-  #   }
-
-  tags = {
-    Name = "Tabla-de-rutas-Privada-1"
-  }
-}
-
-resource "aws_route_table_association" "as_privada_1" {
-  subnet_id      = aws_subnet.subnet_privada_1.id
-  route_table_id = aws_route_table.privada_1.id
-}
-
-resource "aws_route_table" "privada_2" {
-  vpc_id = aws_vpc.vpc-srd.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw_2.id
-  }
-
-  #   route {
-  #     cidr_block = "192.168.0.0/16"
-  #     gateway_id = aws_internet_gateway.igw.id
-  #   }
-
-  tags = {
-    Name = "Tabla-de-rutas-Privada-2"
-  }
-}
-
-resource "aws_route_table_association" "as_privada_2" {
-  subnet_id      = aws_subnet.subnet_privada_2.id
-  route_table_id = aws_route_table.privada_2.id
-}
-
-resource "aws_network_acl" "n_acl" {
-  vpc_id     = aws_vpc.vpc-srd.id
-  subnet_ids = [aws_subnet.subnet_publica_1.id, aws_subnet.subnet_publica_2.id, aws_subnet.subnet_privada_1.id, aws_subnet.subnet_privada_2.id]
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 22
-    to_port    = 22
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 300
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-  //El protocolo 1 es ICMP
-  ingress {
-    protocol   = "1"
-    rule_no    = 400
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 8 // ICMP type for Echo request (ping)
-    to_port    = 0 // ICMP code for Echo request (ping)
-  }
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 22
-    to_port    = 22
-  }
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  egress {
-    protocol   = "tcp"
-    rule_no    = 300
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-  //El protocolo 1 es ICMP
-  egress {
-    protocol   = "1"
-    rule_no    = 400
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0 // ICMP type for Echo reply
-    to_port    = 0 // ICMP code for Echo reply
-  }
-
-  tags = {
-    Name = "N-ACL"
-  }
-}
-
-resource "aws_security_group" "sg_alb" {
-  name        = "sg_alb"
-  description = "Permite trafico HTTP y HTTPS en el ALB"
-  vpc_id      = aws_vpc.vpc-srd.id
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SG-ALB"
-  }
+resource "aws_route_table_association" "public_route_table_association" {
+    subnet_id      = aws_subnet.subnet_publica.id
+    route_table_id = aws_route_table.tabla_ruta_publica.id
 }
 
 resource "aws_security_group" "sg_bastion" {
-  name        = "sg_bastion"
-  description = "Se permite SSH, HTTP y HTTPS en el bastion"
-  vpc_id      = aws_vpc.vpc-srd.id
+    name        = "sg_bastion"
+    description = "Security group Bastion"
+    vpc_id      = aws_vpc.vpc_srd.id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "sg_bastion"
+    }
+
+}
+resource "aws_security_group" "sg_web" {
+  name        = "sg_web"
+  description = "Security group WebServer"
+  vpc_id      = aws_vpc.vpc_srd.id
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.sg_bastion.id]
   }
 
   ingress {
-    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -304,18 +104,9 @@ resource "aws_security_group" "sg_bastion" {
   }
 
   ingress {
-    description = "HTTPS"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "ICMP"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -327,55 +118,30 @@ resource "aws_security_group" "sg_bastion" {
   }
 
   tags = {
-    Name = "SG-Bastion"
+    Name = "sg_web"
   }
 }
 
-resource "aws_security_group" "sg_web" {
-  name        = "sg_web"
-  description = "Permite trafico HTTP, HTTPS. SSH solo desde bastiones"
-  vpc_id      = aws_vpc.vpc-srd.id
+resource "aws_security_group" "sg_rds" {
+    name        = "sg_rds"
+    description = "Security group RDS"
+    vpc_id      = aws_vpc.vpc_srd.id
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    ingress {
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        security_groups = [aws_security_group.sg_web.id]
+    }
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  ingress {
-    description     = "SSH desde bastiones"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    security_groups = [aws_security_group.sg_bastion.id]
-  }
-
-  ingress {
-    description     = "ICMP"
-    from_port       = -1
-    to_port         = -1
-    protocol        = "icmp"
-    security_groups = [aws_security_group.sg_bastion.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "SG-Web"
-  }
+    tags = {
+        Name = "sg_rds"
+    }
 }
