f10af97fe8e6114a1570116046ed312203e8ee16
Mauri <mauricampodonico@outlook.com>
2023-11-27 01:17:38 +0000

    Se agregan automatizaciones

--- instances.tf
+++ instances.tf
@@ -1,78 +1,117 @@
+
+resource "aws_instance" "bastion" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  subnet_id              = aws_subnet.subnet_publica.id
+  vpc_security_group_ids = [aws_security_group.sg_bastion.id]
+  user_data              = <<-EOF
+  #!/bin/bash
+  # SSH Hardening
+  sudo sed -i 's/#Port 22/Port 2939/' /etc/ssh/sshd_config
+  sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+  sudo systemctl restart sshd
+EOF
+
+  tags = {
+    Name = "Bastion"
+  }
+}
+
+resource "aws_eip" "bastion" {
+  instance = aws_instance.bastion.id
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  subnet_id              = aws_subnet.subnet_privada.id
+  private_ip             = "10.0.2.40"
+  vpc_security_group_ids = [aws_security_group.sg_mysql.id]
+  user_data              = <<-USERDATA
+  #!/bin/bash
+  # SSH Hardening
+  sudo sed -i 's/#Port 22/Port 2939/' /etc/ssh/sshd_config
+  sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+  sudo systemctl restart sshd
+
+  sudo apt update -y
+  sudo apt install mysql-server -y
+  sudo systemctl start mysql
+  sudo systemctl enable mysql
+
+  # Crear base de datos para wp
+  sudo mysql -e ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password by 'S3gurid4d';
+  sudo mysql -e "CREATE DATABASE wp;"
+  sudo mysql -e "CREATE USER 'wp_user'@'%' IDENTIFIED BY 'Obli.SRD-2023';"
+  sudo mysql -e "GRANT ALL PRIVILEGES ON wp.* TO 'wp_user'@'%';"
+  sudo mysql -e "FLUSH PRIVILEGES;"
+USERDATA
+#PARA QUE MYSQL ACEPTE CONEXIONES EXTERNAS HAY QUE MODIFICAR EL ARCHIVO /etc/mysql/mysql.conf.d/mysqld.cnf Y CAMBIAR EL BIND
+#ADDRESS DE 127.0.0.1 A 0.0.0.0, LUEGO REINICIAR EL SERVICIO CON sudo systemctl restart mysql
+  tags = {
+    Name = "MySQL"
+  }
+
+  depends_on = [
+    aws_nat_gateway.nat, aws_route_table_association.as_tabla_ruta_privada
+  ]
+}
+
+resource "aws_instance" "webserver" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  subnet_id              = aws_subnet.subnet_publica.id
+  vpc_security_group_ids = [aws_security_group.sg_web.id]
+  user_data              = <<-EOF
+    #!/bin/bash
+    # SSH Hardening
+    sudo sed -i 's/#Port 22/Port 2939/' /etc/ssh/sshd_config
+    sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin no/' /etc/ssh/sshd_config
+    sudo systemctl restart sshd
+    
+    sudo apt update -y
+    sudo apt install apache2 -y
+    sudo systemctl enable apache2
+    sudo systemctl start apache2
+    sudo apt install mysql-server php libapache2-mod-php php-mysql -y
+    wget https://wordpress.org/latest.tar.gz
+    tar xzvf latest.tar.gz
+    sudo mv wordpress/ /var/www/html/
+    sudo systemctl restart apache2
+    
+    # Configura WordPress para usar MySQL server
+    sudo sed -i "s/database_name_here/wp/g" /var/www/html/wordpress/wp-config-sample.php
+    sudo sed -i "s/username_here/wp_user/g" /var/www/html/wordpress/wp-config-sample.php
+    sudo sed -i "s/password_here/Obli.SRD-2023/g" /var/www/html/wordpress/wp-config-sample.php
+    sudo sed -i "s/localhost/${aws_instance.mysql.private_ip}/g" /var/www/html/wordpress/wp-config-sample.php
+    sudo mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+    sudo systemctl restart apache2
+EOF
+#PARA QUE AL INGRESAR AL SERVIDOR WEB VIA NAVEGADOR Y QUE NOS MUESTRE LA PAGINA DE WORDPRESS HAY QUE MODIFICAR
+#EL ARCHIVO /etc/apache2/sites-available/000-default.conf Y MODIFICAR LA LÍNEA DocumentRoot /var/www/html POR 
+#DocumentRoot /var/www/html/wordpress, LUEGO REINICIAR EL SERVICIO CON sudo systemctl restart apache2
+  tags = {
+    Name = "WebServer"
+  }
+
+  depends_on = [
+    aws_instance.mysql
+  ]
+}
+
+resource "aws_eip" "webserver" {
+  instance = aws_instance.webserver.id
+
+  depends_on = [
+    aws_instance.webserver
+  ]
+}
+
 resource "aws_key_pair" "key_pair" {
   key_name   = "id_rsa.pub"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzMeLrNAK40kahNSY5MVQ3dIKeHqWXD/uXrkL6dUmW/R7q4FUfgpUESAfPLQVbw+UVtvJcNjVe6OTZk3U4S7L0J0vRw6U+LClYJmqOV62HePDiD2/Xvs6AAhf4Kh+E3ExjBI8afRQQFnGXjeV25OULeRk4vcc2PBgPMJj3yw88Ld6qWpca2OCQ0bSKFv5tYm4/ZnHC9KcFTPjCpErxDTa2S6TvtZ0jmhthBdw9TEJuTA3YxLOXBTUUPJ9nm9YDXycAMzTnhCtgsTzXLYAN902xmT2aZndGKpKAdduVaxpULOGMGzSuZ930prohrA21lz5fTYrgq4iSHBs1OtS9DYZ4Q1xwo39bHbbeqnscEV0Q8YGOt4S+sH8wUbWR5uHdgzYHqw8uVqtwnzo03jpk7O8AFMuNnL54CQZUEurhpE6il8PbGlvFCF+E9N+uQUlRaBTUCMGccbm9Vid6VizJZNCmQvBPoS4pRlvW6NavA/OHfblBi2BsC7VP6RclwQT7Ikc= srd@obli-srd"
 }
-
-resource "aws_instance" "bastion" {
-    ami           = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-    key_name      = aws_key_pair.key_pair.key_name
-    subnet_id     = aws_subnet.subnet_publica.id
-    vpc_security_group_ids = [aws_security_group.sg_bastion.id]
-
-    tags = {
-        Name = "Bastion"
-    }
-}
-
-resource "aws_instance" "webserver" {
-    ami           = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-    key_name      = aws_key_pair.key_pair.key_name
-    subnet_id     = aws_subnet.subnet_publica.id
-    vpc_security_group_ids = [aws_security_group.sg_web.id]
-
-    user_data = <<-EOF
-                            #!/bin/bash
-                            sudo apt update -y
-                            sudo apt install apache2 -y
-                            sudo systemctl enable apache2
-                            sudo systemctl start apache2
-                            sudo apt install mysql-server php libapache2-mod-php php-mysql -y
-                            wget https://wordpress.org/latest.tar.gz
-                            tar xzvf latest.tar.gz
-                            sudo cp -R wordpress/* /var/www/html/
-                            EOF
-
-    tags = {
-        Name = "WebServer"
-    }
-}
-
-resource "aws_db_instance" "WP_DB" {
-    allocated_storage    = 20
-    storage_type         = "gp2"
-    engine               = "mysql"
-    engine_version       = "5.7"
-    instance_class       = "db.t2.micro"
-    identifier           = "srd"
-    username             = "Obli.SRD"
-    password             = "Obli.SRD-2023"
-    parameter_group_name = "default.mysql5.7"
-    vpc_security_group_ids = [aws_security_group.sg_rds.id]
-    db_subnet_group_name = "db_subnet_group" 
-
-    tags = {
-        Name = "WP_DB"
-    }
-}
-
-resource "null_resource" "configurar_wordpress" {
-    depends_on = [aws_instance.webserver, aws_db_instance.WP_DB]
-
-    provisioner "remote-exec" {
-        inline = [
-            "sudo sed -i 's/database_name_here/srd/g' /var/www/html/wp-config.php",
-            "sudo sed -i 's/username_here/Obli.SRD/g' /var/www/html/wp-config.php",
-            "sudo sed -i 's/password_here/Obli.SRD-2023/g' /var/www/html/wp-config.php",
-            "sudo sed -i 's/localhost/${aws_db_instance.WP_DB.endpoint}/g' /var/www/html/wp-config.php"
-        ]
-
-        connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = file("~/.ssh/id_rsa")
-            host        = aws_instance.webserver.public_ip
-        }
-    }
-}
+#SI DESEA CONECTARSE AL SERVIDOR WEB VIA SSH, DEBE MODIFICAR EL ATRIBUTO public_key CON SU LLAVE PÚBLICA
--- network.tf
+++ network.tf
@@ -1,87 +1,121 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_vpc" "vpc_srd" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-        Name = "vpc-srd"
-    }
+  tags = {
+    Name = "vpc-srd"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vpc_srd.id
+  vpc_id = aws_vpc.vpc_srd.id
+
+  tags = {
+    Name = "igw"
+  }
 }
 
 resource "aws_subnet" "subnet_publica" {
-    vpc_id     = aws_vpc.vpc_srd.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
+  vpc_id                  = aws_vpc.vpc_srd.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
-    tags = {
-        Name = "subnet-publica"
-    }
+  tags = {
+    Name = "subnet-publica"
+  }
 }
 
 resource "aws_subnet" "subnet_privada" {
-    vpc_id     = aws_vpc.vpc_srd.id
-    cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.vpc_srd.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
 
-    tags = {
-        Name = "subnet-privada"
-    }
+  tags = {
+    Name = "subnet-privada"
+  }
 }
 
-resource "aws_db_subnet_group" "db_subnet_group" {
-    name       = "db_subnet_group"
-    subnet_ids = [aws_subnet.subnet_privada.id]
+resource "aws_eip" "nat" {
+  vpc = true
 
-    tags = {
-        Name = "DB Subnet Group"
-    }
+  tags = {
+    Name = "NAT Privado"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnet_publica.id
+
+  tags = {
+    Name = "NAT Privado"
+  }
 }
 
 resource "aws_route_table" "tabla_ruta_publica" {
-    vpc_id = aws_vpc.vpc_srd.id
+  vpc_id = aws_vpc.vpc_srd.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
-    tags = {
-        Name = "tabla_ruta_publica"
-    }
+  tags = {
+    Name = "tabla_ruta_publica"
+  }
 }
 
-resource "aws_route_table_association" "public_route_table_association" {
-    subnet_id      = aws_subnet.subnet_publica.id
-    route_table_id = aws_route_table.tabla_ruta_publica.id
+resource "aws_route_table_association" "as_tabla_ruta_publica" {
+  subnet_id      = aws_subnet.subnet_publica.id
+  route_table_id = aws_route_table.tabla_ruta_publica.id
 }
+
+resource "aws_route_table" "tabla_ruta_privada" {
+  vpc_id = aws_vpc.vpc_srd.id
+
+  tags = {
+    Name = "tabla_ruta_privada"
+  }
+}
+
+resource "aws_route" "nat_gateway_privado" {
+  route_table_id         = aws_route_table.tabla_ruta_privada.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "as_tabla_ruta_privada" {
+  subnet_id      = aws_subnet.subnet_privada.id
+  route_table_id = aws_route_table.tabla_ruta_privada.id
+}
+
 
 resource "aws_security_group" "sg_bastion" {
-    name        = "sg_bastion"
-    description = "Security group Bastion"
-    vpc_id      = aws_vpc.vpc_srd.id
+  name        = "sg_bastion"
+  description = "Security group Bastion"
+  vpc_id      = aws_vpc.vpc_srd.id
 
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 2939 # SSH hardening
+    to_port     = 2939
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "sg_bastion"
-    }
+  tags = {
+    Name = "sg_bastion"
+  }
 
 }
 resource "aws_security_group" "sg_web" {
@@ -90,9 +124,9 @@ resource "aws_security_group" "sg_web" {
   vpc_id      = aws_vpc.vpc_srd.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port       = 2939 # SSH hardening
+    to_port         = 2939
+    protocol        = "tcp"
     security_groups = [aws_security_group.sg_bastion.id]
   }
 
@@ -122,26 +156,30 @@ resource "aws_security_group" "sg_web" {
   }
 }
 
-resource "aws_security_group" "sg_rds" {
-    name        = "sg_rds"
-    description = "Security group RDS"
-    vpc_id      = aws_vpc.vpc_srd.id
+resource "aws_security_group" "sg_mysql" {
+  name        = "mysql"
+  description = "Security group MySQL"
+  vpc_id      = aws_vpc.vpc_srd.id
 
-    ingress {
-        from_port   = 3306
-        to_port     = 3306
-        protocol    = "tcp"
-        security_groups = [aws_security_group.sg_web.id]
-    }
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_web.id]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port       = 2939 # SSH hardening
+    to_port         = 2939
+    protocol        = "tcp"
+    security_groups = [aws_security_group.sg_bastion.id]
+  }
 
-    tags = {
-        Name = "sg_rds"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
